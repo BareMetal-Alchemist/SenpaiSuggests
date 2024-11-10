3f9cbbe2225e39aa07a67a2e12c9728808fa3968
Quentin Rivest <queriv47x@csu.fullerton.edu>
2024-11-10 12:15:08 -0800

    Edited the NODE_ENV line of shell.nix to get rid of bash message

--- SSCodeBase/client/src/App.js
+++ SSCodeBase/client/src/App.js
@@ -2,26 +2,27 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import GoogleAuth from './Components/GoogleAuth.jsx';
 import WishListPage from './pages/WishListPage.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
-import LoginPage from './pages/loginPage';
+import Login from './pages/loginPage';
 import AnimeList from './pages/animelist';
 
 
 const App = () => {
   return (
-/*
+
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />}/>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/animelist" element={<AnimeList />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
-*/
 
+/*
     <>
       <WishListPage />
     </>
+*/
   );
 };
 
--- SSCodeBase/client/src/pages/loginPage.jsx
+++ SSCodeBase/client/src/pages/loginPage.jsx
@@ -4,16 +4,12 @@ import VideoBackground from '../Components/videobackground';
 import LoginForm from '../Components/loginForm';
 import './loginPage.css';
 
-function Login() {
+export default function Login() {
   return (
-
-
       <div className="hello">
       <h1 id="title">Senpai Suggests</h1>
         <LoginForm />
         <VideoBackground />
       </div>
-
   );
-
 }
